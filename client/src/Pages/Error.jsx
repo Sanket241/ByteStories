d7956e3a87c941948a5ba636506f2999eef80bcf
Sanket <sanketsharma241@gmail.com>
2024-06-07 18:47:53 +0530

    adding routing and header section

---
+++ client/src/Pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+    <h1 className="text-6xl font-bold text-red-500">404</h1>
+    <p className="mt-4 text-xl">Sorry, we couldn't find what you were looking for.</p>
+  </div>
+  )
+}
+
+export default Error
