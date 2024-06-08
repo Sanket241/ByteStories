39f85272ee1d10487ff78ec375945b8fd6831529
Sanket <sanketsharma241@gmail.com>
2024-06-08 11:21:21 +0530

    adding footer section

--- client/src/Component/Footer.jsx
+++ client/src/Component/Footer.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from 'react';
+import { Footer } from 'flowbite-react';
+import { NavLink } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
 
-const Footer = () => {
+export default function FooterCom() {
   return (
-    <div>Footer</div>
-  )
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className="flex-1">
+            <NavLink to='/' className='font-bold dark:text-white text-4xl'>
+              <span className='text-gray-500'>Byte</span>
+              <span className='text-gray-700'>Stories</span>
+            </NavLink>
+            <p className='text-sm mt-5'>
+              You can sign up with your email and password or with Google.
+            </p>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                 ByteStories
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com/Sanket241'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='https://discord.com/channels/@me/1246357067166384169'>Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="ByteStories"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook} />
+            <Footer.Icon href='#' icon={BsInstagram} />
+            <Footer.Icon href='https://twitter.com/Sankets_' icon={BsTwitter} />
+            <Footer.Icon href='https://github.com/Sanket241' icon={BsGithub} />
+            <Footer.Icon href='#' icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
-
-export default Footer
