671dd0d8340be79cae020ef2c8b03cbaf52c2140
Sanket <sanketsharma241@gmail.com>
2024-06-12 08:15:03 +0530

    complete profile page ui

--- client/src/Pages/Dashboards.jsx
+++ client/src/Pages/Dashboards.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashboardSidebar from '../Component/DashboardSidebar';
+import DashboardProfile from '../Component/DashboardProfile';
 const Dashboards = () => {
+
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>Dashboards</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="mid:w-56">
+        <DashboardSidebar />
+      </div>
+      {
+        tab === 'profile' && <DashboardProfile />
+      }
+    </div>
   )
 }
 
