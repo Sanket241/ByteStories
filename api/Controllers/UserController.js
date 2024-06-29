1d7f1fcb7ed1820db757da4fa37da5da8d6758b3
Sanket <sanketsharma241@gmail.com>
2024-06-29 22:23:07 +0530

    add routes

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -2,3 +2,9 @@ export const test = async (req, res, next) => {
     console.log('test route user');
     res.send('Test route user');
 }
+
+export const updateUser = async(req,resp,next)=>{
+    console.log('update user');
+    resp.send('update user');
+    console.log(req.user)
+}
--- api/Routes/UserRoute.js
+++ api/Routes/UserRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { test } from '../Controllers/UserController.js';
+import { test, updateUser } from '../Controllers/UserController.js';
+
+import { verifyToken } from '../Utills/verifyUser.js';
 
 const router = express.Router();
 
 
 router.get('/test', test)
+router.route('/update:userId').put(verifyToken, updateUser)
 
 export default router;
