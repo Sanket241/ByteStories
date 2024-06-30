4e97937b0d01fe8e1385fc040b5ffecb7beea6e1
Sanket <sanketsharma241@gmail.com>
2024-06-30 13:20:54 +0530

    adding route changes

--- api/Routes/UserRoute.js
+++ api/Routes/UserRoute.js
@@ -6,7 +6,8 @@ import { verifyToken } from '../Utills/verifyUser.js';
 const router = express.Router();
 
 
-router.get('/test', test)
+// router.get('/test', test)
+router.route('/test').get(test)
 router.route('/update:userId').put(verifyToken, updateUser)
 
 export default router;
