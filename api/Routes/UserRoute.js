3cc3da52bd23b7bb23990c167e68dcfd6a2b26e4
Sanket <sanketsharma241@gmail.com>
2024-07-02 22:17:52 +0530

    update user api route

--- api/Routes/UserRoute.js
+++ api/Routes/UserRoute.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { test, updateUser } from '../Controllers/UserController.js';
+import { test, updateUser, deleteUser } from '../Controllers/UserController.js';
 
 import { verifyToken } from '../Utills/verifyUser.js';
 
 const router = express.Router();
 
 
-// router.get('/test', test)
-router.route('/test').get(test)
-router.route('/update:userId').put(verifyToken, updateUser)
+router.get('/test', test)
+router.put('/update/:userId', verifyToken, updateUser)
+router.delete('/delete',deleteUser)
+// router.route('/test').get(test)
+// router.route('/update/:userId').put(verifyToken, updateUser)
 
 export default router;
--- api/Utills/verifyUser.js
+++ api/Utills/verifyUser.js
@@ -1,13 +1,15 @@
-import jwt from 'jsonwebtoken'
-import { Errorhandler } from './Error.js'
+import jwt from 'jsonwebtoken';
+import { Errorhandler } from './Error.js';
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    if (!token) return next(Errorhandler(401, 'Access Denied'));
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return next(Errorhandler(403, 'Unauthorized Access'))
-        }
-        req.user = user;
-        next();
-    })
-}
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(Errorhandler(401, 'Unauthorized'));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(Errorhandler(401, 'Unauthorized'));
+    }
+    req.user = user;
+    next();
+  });
+};
