baf2e12529086891534a002e2599254d40c1f0c0
Sanket <sanketsharma241@gmail.com>
2024-07-08 23:48:35 +0530

    api post route

--- api/Routes/PostRoute.js
+++ api/Routes/PostRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createPost } from '../Controllers/PostController.js';
+import { createPost, getPost } from '../Controllers/PostController.js';
 import { verifyToken } from '../Utills/verifyUser.js';
 
 const router = express.Router();
 
 router.route('/create').post(verifyToken, createPost);
+router.route('/getpost').get(getPost);
 
 export default router
