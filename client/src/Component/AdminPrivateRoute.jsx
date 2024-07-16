be5e074435ed3e69becf8c8efe5fc22f0b10db70
Sanket <sanketsharma241@gmail.com>
2024-07-16 22:16:32 +0530

    effect

--- client/src/Component/AdminPrivateRoute.jsx
+++ client/src/Component/AdminPrivateRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet, Navigate } from 'react-router-dom'
 const AdminPrivateRoute = () => {
+    
     const { currentUser } = useSelector(state => state.user)
     return currentUser && currentUser.isAdmin ? <Outlet /> : <Navigate to='/signin' />
 
