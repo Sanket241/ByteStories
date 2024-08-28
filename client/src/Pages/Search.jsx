fa31cab2b2063bdd552e5b7214a1bc706805b573
Sanket <sanketsharma241@gmail.com>
2024-08-28 12:25:56 +0530

    error fix

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='startupstories'>Startup Stories</option>
+              <option value='technologies'>New Technologies</option>
+              <option value='research'>New Research</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
